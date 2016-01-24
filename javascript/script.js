53e502a0ba9681422ccee1726f146da5123d6a3b
Klajdi Bejko <kbejko@gmail.com>
2016-01-24 00:17:37 -0500

    Work in progress to make it responsive

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,11 @@
-(function rotateHamburger(){
-   var burger = document.querySelector(".burger")
-   var first = document.querySelector(".first")
-   var second = document.querySelector(".second")
-   var ul = document.querySelector(".nav-links")
-   burger.onclick = function(){
-     first.classList.toggle("rotate-down")
-     second.classList.toggle("rotate-up")
-     ul.classList.toggle("visible")
-   }
-})()
+// (function rotateHamburger(){
+//    var burger = document.querySelector(".burger")
+//    var first = document.querySelector(".first")
+//    var second = document.querySelector(".second")
+//    var ul = document.querySelector(".nav-links")
+//    burger.onclick = function(){
+//      first.classList.toggle("rotate-down")
+//      second.classList.toggle("rotate-up")
+//      ul.classList.toggle("visible")
+//    }
+// })()
