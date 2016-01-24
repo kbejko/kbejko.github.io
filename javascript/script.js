568827c1ae353fcbf541bc0264bee2dbc2100c3a
Klajdi Bejko <kbejko@gmail.com>
2016-01-24 16:37:39 -0500

    Makes content more responsive (WIP)

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,11 @@
-(function rotateHamburger(){
-   var burger = document.querySelector(".burger")
-   var first = document.querySelector(".first")
-   var second = document.querySelector(".second")
-   var sidebar = document.querySelector(".sidebar")
-   burger.onclick = function(){
-     first.classList.toggle("rotate-down")
-     second.classList.toggle("rotate-up")
-     sidebar.classList.toggle("visible")
-   }
-})()
+// (function rotateHamburger(){
+//    var burger = document.querySelector(".burger")
+//    var first = document.querySelector(".first")
+//    var second = document.querySelector(".second")
+//    var sidebar = document.querySelector(".sidebar")
+//    burger.onclick = function(){
+//      first.classList.toggle("rotate-down")
+//      second.classList.toggle("rotate-up")
+//      sidebar.classList.toggle("visible")
+//    }
+// })()
