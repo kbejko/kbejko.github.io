65f9d50f8fbdf1a7d1353f0c732778ecf7373bab
Klajdi Bejko <kbejko@gmail.com>
2016-02-03 14:22:01 -0500

    Add jquery functionality to highlight current tab. Create content for index page

--- javascript/script.js
+++ javascript/script.js
@@ -9,3 +9,20 @@
      contact.classList.toggle("visible")
    }
 })()
+
+$(document).ready(function(){
+  var home = $(".home")
+  var journal = $(".journal")
+  var resources = $(".resources")
+
+  if (window.location.pathname === "/journal/") {
+    home.removeClass("active")
+    resources.removeClass("active")
+    journal.addClass("active")
+  } else if (window.location.pathname === "/resources/") {
+    home.removeClass("active")
+    resources.addClass("active")
+  } else {
+    home.addClass("active")
+  }
+})
