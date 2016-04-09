3c63d330b21055d320c7c2f3c71cbccf73c6b8e8
Klajdi Bejko <kbejko@gmail.com>
2016-04-08 22:27:56 -0400

    in process of restyling

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,18 @@
 (function rotateHamburger(){
-   var burger = document.querySelector(".burger")
-   var first = document.querySelector(".first")
-   var second = document.querySelector(".second")
-   var contact = document.querySelector(".contact")
+   var burger = document.querySelector("#burger")
+   var first = document.querySelector(".line-top")
+   var second = document.querySelector(".line-bottom")
+   var nav = document.querySelector(".site__nav")
    burger.onclick = function(){
      first.classList.toggle("rotate-down")
      second.classList.toggle("rotate-up")
-     contact.classList.toggle("visible")
+     nav.classList.toggle("visible")
    }
 })();
 
 // Checks current path name and matches it to li class name to add class active
 (function underlineCurrentTab(){
-  var navLinks = document.querySelector('.nav-links').children
+  var navLinks = document.querySelector('.site__nav--links').children
   var pathName = location.pathname.split('/')[1]
 
   for (var i = 0; i < navLinks.length; i++) {
