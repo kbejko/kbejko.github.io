6a9bfac416108424675ffb1d183a9d58d3a9071e
Klajdi Bejko <kbejko@gmail.com>
2016-04-10 19:52:48 -0400

    figuring out to transition between pages

--- javascript/script.js
+++ javascript/script.js
@@ -14,12 +14,34 @@
 (function underlineCurrentTab(){
   var navLinks = document.querySelectorAll('.site__nav--links li a')
   var pathName = location.pathname
-
+  console.log(navLinks)
   for (var i = 0; i < navLinks.length; i++) {
     if (pathName === navLinks[i].pathname) {
       navLinks[i].classList.add('active')
     }
   }
+})();
+
+(function pageTransition(){
+  var navLinks = document.querySelectorAll('.site__nav--links li a')
+  var nav = document.querySelector('.site__nav')
+  var img = document.querySelector('.page__content')
+
+  // setTimeout(function(){
+  //   navLinks[1].addEventListener('click', function(){
+  //     nav.removeClass('active')
+  //     console.log('blah')
+  //   }, 3000)
+  // })
+  navLinks[0].addEventListener('click', function(evt){
+
+    evt.preventDefault()
+    nav.classList.remove('visible')
+    setTimeout(function(){
+      img.classList.add('show')
+    }, 1500)
+  })
+
 })();
 
 console.log('Hire me :)')
